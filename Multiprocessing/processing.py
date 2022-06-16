c19da7bc77d29d6f7c85fc935648dda47c1b4f8e
SylviaWhittle <sylviwhittle@gmail.com>
2022-06-16 20:51:58 +0100

    Add multiprocessing experiments

--- Multiprocessing/processing.py
+++ Multiprocessing/processing.py
@@ -1,2 +1,19 @@
-import multiprocessing
+import threading
+import logging
+import time
 
+def thread_function(name):
+    logging.info(f'thread {name} starting')
+    time.sleep(3)
+    logging.info(f'thread {name} finished')
+
+if __name__=='__main__':
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    logging.info('main : before running thread')
+    x = threading.Thread(target=thread_function, args=(1,))
+    x.start()
+    logging.info('main : wait for thread to finish')
+    x.join()
+    logging.info('main : all done')
